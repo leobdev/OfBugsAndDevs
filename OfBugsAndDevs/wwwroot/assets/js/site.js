e8d207771c05bce1d46270665fc479d7df2e7da7
Leo Barnuevo <leocoding.net@gmail.com>
2024-10-29 07:26:54 -0500

    + Implemented prismjs to wirk  with quill rich editor

---
+++ OfBugsAndDevs/wwwroot/assets/js/site.js
@@ -0,0 +1,37 @@
+﻿
+window.initializeQuill = () => {
+    document.addEventListener('DOMContentLoaded', function () {
+        var quill = new Quill('#editor', {
+            theme: 'snow',
+            modules: {
+                syntax: true,
+                toolbar: [
+                    [{ 'header': [1, 2, false] }],
+                    ['bold', 'italic', 'underline'],
+                    ['code-block']
+                ]
+            }
+        });
+
+        quill.on('text-change', function (delta, oldDelta, source) {
+            setTimeout(() => {
+                document.querySelectorAll('pre code').forEach((block) => {
+                    Prism.highlightElement(block);
+                });
+            }, 0);
+        });
+
+        setTimeout(() => {
+            document.querySelectorAll('pre code').forEach((block) => {
+                Prism.highlightElement(block);
+            });
+        }, 0);
+    });
+};
+
+// Function to apply Prism syntax highlighting
+function applyPrismHighlighting() {
+    document.querySelectorAll('pre code').forEach((block) => {
+        Prism.highlightElement(block);
+    });
+}
