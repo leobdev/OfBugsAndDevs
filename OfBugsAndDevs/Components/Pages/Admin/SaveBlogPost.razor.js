17a1b21e69491bbe87750ae8a87b2e792e33c08d
Leo Barnuevo <leocoding.net@gmail.com>
2024-11-05 07:25:40 -0600

    + added saveblogpost js file

---
+++ OfBugsAndDevs/Components/Pages/Admin/SaveBlogPost.razor.js
@@ -0,0 +1,35 @@
+﻿export function onLoad() {
+    console.log('Loaded');
+
+    document.addEventListener("DOMContentLoaded", function () {
+        if (typeof hljs !== 'undefined') {
+            hljs.configure({ languages: ['csharp'] }); // Specify C# language for highlighting
+
+            function applySyntaxHighlighting(editor) {
+                editor.querySelectorAll('pre').forEach(block => {
+                    hljs.highlightElement(block);
+                });
+            }
+
+            window.applyHighlightingToQuillContent = (containerId) => {
+                const container = document.getElementById(containerId);
+                if (container) {
+                    const editor = container.querySelector('.ql-editor');
+                    if (editor) applySyntaxHighlighting(editor);
+                }
+            };
+        }
+    });
+}
+
+export function onUpdate() {
+    console.log('Updated');
+}
+
+export function onDispose() {
+    console.log('Disposed');
+}
+
+
+
+
